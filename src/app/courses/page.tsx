f19709af6b8378044a18f99901a16a66b9986f97
Samrat Neupane <71063668+SamratNeupane787@users.noreply.github.com>
2024-05-05 22:43:01 +0545

    added key

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -14,7 +14,7 @@ export default function courseCard() {
       <div className=" grid grid-cols-1 gap-3 place-items-center mx-10 md:grid-cols-2 ">
         {courseData.courses.map((item, index) => {
           return (
-            <CardContainer className="inter-var">
+            <CardContainer key={index} className="inter-var">
               <CardBody className="bg-gray-50 relative group/card  dark:hover:shadow-2xl dark:hover:shadow-emerald-500/[0.1] dark:bg-black dark:border-white/[0.2] border-black/[0.1] w-auto sm:w-[30rem] h-auto rounded-xl p-6 border  ">
                 <CardItem
                   translateZ="50"
